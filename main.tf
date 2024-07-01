a4c1b48478585e351cb3edd1f7367a5b10ae6d13
udaynavuluri <udaynaninavuluri@gmail.com>
2024-07-01 11:38:27 +0530

    v2

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = ap-south-1
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = ami-01b799c439fd5516a
-  instance_type = t2.micro
+  ami           = "ami-01b799c439fd5516a"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "example-instance"
